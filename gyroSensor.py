bd5057c6d788be8db54f1e308cb4f08b538e5bb6
Emily Cottle <32680215+ecottle@users.noreply.github.com>
2020-04-13 16:45:16 -0500

    Create gyroSensor.py

---
+++ gyroSensor.py
@@ -0,0 +1,10 @@
+def initialize():
+  #initialize digital inputs
+  return 1
+
+def rotation():
+  # return the input read for rotation
+  g = input("Basket Rotation: ")
+  print(g)
+  g = int(g)
+  return g
